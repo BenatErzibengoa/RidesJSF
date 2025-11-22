93e80cc3c5a45ad5cf6ed02d5580ff98b60116f5
Beñat <bercibengoa001@ikasle.ehu.eus>
2025-11-22 12:10:52 +0100

    1. zatia

---
+++ src/main/java/bean/CreateRideBean.java
@@ -0,0 +1,125 @@
+package bean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import businessLogic.BLFacade;
+import domain.Driver;
+import exceptions.RideAlreadyExistException;
+import exceptions.RideMustBeLaterThanTodayException;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
+@Named("createride")
+@SessionScoped
+public class CreateRideBean implements Serializable {
+
+	private String departCity;
+	private String arrivalCity;
+	private int numSeats;
+	private float price;	
+	private Date rideDate;
+	
+	private Driver driver;
+	
+	public CreateRideBean() {
+        // Beharrezkoa
+    }
+
+	public String createRide() {
+        try {
+            BLFacade facade = FacadeBean.getBusinessLogic();
+            String driverEmail = "driver1@gmail.com"; 
+            facade.createRide(departCity, arrivalCity, rideDate, numSeats, price, driverEmail);
+
+            FacesContext.getCurrentInstance().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Ride created successfully: " + departCity + " -> " + arrivalCity));
+            
+            System.out.println("Orain, horrela geratzen dira departcities: " + facade.getDepartCities());
+            this.departCity = "";
+            this.arrivalCity = "";
+            this.numSeats = 0;
+            this.price = 0;
+            this.rideDate = null;
+
+            return ""; 
+
+        } catch (RideMustBeLaterThanTodayException e) {
+            FacesContext.getCurrentInstance().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Date must be later than today"));
+            return null;
+        } catch (RideAlreadyExistException e) {
+            FacesContext.getCurrentInstance().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Ride already exists."));
+            return null;
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, 
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Unexpected error: " + e.getMessage()));
+            return null;
+        }
+    }
+	
+
+	public Date getMinDate() {
+	    Calendar c = Calendar.getInstance();
+	    c.setTime(new Date());
+	    c.add(Calendar.DATE, 1); 
+	    return c.getTime(); // Biharko eguna itzuli
+	}
+	
+	public String getDepartCity() {
+		return departCity;
+	}
+
+	public void setDepartCity(String departCity) {
+		this.departCity = departCity;
+	}
+
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+
+	public int getNumSeats() {
+		return numSeats;
+	}
+
+	public void setNumSeats(int numSeats) {
+		this.numSeats = numSeats;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	public Date getRideDate() {
+        return rideDate;
+    }
+
+    public void setRideDate(Date rideDate) {
+        this.rideDate = rideDate;
+    }
+
+	public Driver getDriver() {
+		return driver;
+	}
+
+	public void setDriver(Driver driver) {
+		this.driver = driver;
+	}
+	
+	
+	
+}
---
+++ src/main/java/bean/QueryRidesBean.java
@@ -0,0 +1,114 @@
+package bean; 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.faces.view.ViewScoped; 
+import jakarta.inject.Named;
+
+import org.primefaces.event.SelectEvent;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+    
+import businessLogic.BLFacade;
+import domain.Ride;
+
+
+@Named("queryrides")
+@ViewScoped  //Datuak bizirik soilik uneko pantailan
+public class QueryRidesBean implements Serializable {
+
+    private String selectedOrigin;
+    private String selectedDestination;
+    private Date selectedDate;
+
+    private List<String> departCities;
+    private List<String> destinationCities;
+    private List<Ride> foundRides;
+
+    private BLFacade facadeBL;
+
+    public QueryRidesBean() {
+    	departCities = new ArrayList<>();
+    	destinationCities = new ArrayList<>();
+    	foundRides = new ArrayList<>();
+    }
+    
+    @PostConstruct
+    public void init() {
+        try {
+            facadeBL = FacadeBean.getBusinessLogic();
+            if (facadeBL != null) {
+                departCities = facadeBL.getDepartCities();
+            	System.out.println("Depart cities loaded: " + departCities.toString());
+            } else {
+                System.out.println("Error: FacadeBean returned null. There is no business logic");
+                FacesContext.getCurrentInstance().addMessage(null, 
+                    new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error", "Business Logic not initialized"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+
+    public String searchRides() {
+        if (selectedOrigin != null && selectedDestination != null && selectedDate != null) {
+            
+            foundRides = facadeBL.getRides(selectedOrigin, selectedDestination, selectedDate);
+            
+            if (foundRides.isEmpty()) {
+                // Rides ez --> errore mezua
+                FacesContext.getCurrentInstance().addMessage(null, 
+                    new FacesMessage("Rides not found for these credentials"));
+            }
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, 
+                    new FacesMessage(FacesMessage.SEVERITY_WARN, "Please select origin, destination and date.", null));
+        }
+        
+        return null; // Errorea --> ez egin ezer
+    }
+
+    public void onOriginChange() {
+        if (selectedOrigin != null && !selectedOrigin.isEmpty()) {
+            destinationCities = facadeBL.getDestinationCities(selectedOrigin);
+        }
+        foundRides = null; // Bestela, taula garbitu
+    }
+    
+    public void onDateSelect(SelectEvent<Date> event) {
+        FacesContext.getCurrentInstance().addMessage(null, 
+            new FacesMessage("Selected date: " + event.getObject().toString()));
+    }
+
+	public Date getMinDate() {
+	    Calendar c = Calendar.getInstance();
+	    c.setTime(new Date());
+	    c.add(Calendar.DATE, 1); 
+	    return c.getTime(); // Biharko eguna itzuli
+	}
+
+
+    public String getSelectedOrigin() { return selectedOrigin; }
+    public void setSelectedOrigin(String selectedOrigin) { this.selectedOrigin = selectedOrigin; }
+
+    public String getSelectedDestination() { return selectedDestination; }
+    public void setSelectedDestination(String selectedDestination) { this.selectedDestination = selectedDestination; }
+
+    public Date getSelectedDate() { return selectedDate; }
+    public void setSelectedDate(Date selectedDate) { this.selectedDate = selectedDate; }
+
+    public List<String> getDepartCities() { return departCities; }
+    public void setDepartCities(List<String> departCities) { this.departCities = departCities; }
+
+    public List<String> getDestinationCities() { return destinationCities; }
+    public void setDestinationCities(List<String> destinationCities) { this.destinationCities = destinationCities; }
+
+    public List<Ride> getFoundRides() { return foundRides; }
+    public void setFoundRides(List<Ride> foundRides) { this.foundRides = foundRides; }
+}
